75226f4d5105b9fba5f6183be97c20857aa276e5
chanpreet singh <chanpreetsingh73553@gmail.com>
2024-07-09 19:51:09 +0530

    add font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans: ['Work Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
