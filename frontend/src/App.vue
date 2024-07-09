6e3ca242f364bd42dbe636be6a7ff449ed5ae6fe
chanpreet singh <chanpreetsingh73553@gmail.com>
2024-07-09 20:17:20 +0530

    add comonents in app file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import NavBar from '@/components/NavBar.vue';
+import Footer from "@/components/Footer.vue";
+</script>
+
 <template>
-  <RouterView />
+  <NavBar />
+  <main class="lg:max-w-7xl mx-auto py-8">
+    <RouterView />
+  </main>
+  <Footer />
 </template>
