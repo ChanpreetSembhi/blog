e77433041c0b003e613da25c2f982b99164cb2fa
chanpreet singh <chanpreetsingh73553@gmail.com>
2024-07-09 20:15:15 +0530

    create home page

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Header from '@/components/Header.vue';
+import BlogListings from '@/components/BlogListings.vue';
+</script>
+<template>
+  <Header />
+  <BlogListings :limit="3" :show-button="true" isHome />
+</template>
--- frontend/src/views/HomeView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <h1 class="text-3xl font-bold underline">
-    This is an home page
-  </h1>
-</template>
